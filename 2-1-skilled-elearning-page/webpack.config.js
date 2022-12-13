9dfc5aff46d98801214002d8bbd7512c9cc3b8fb
Kitketovsky <kitketovsky@gmail.com>
2022-12-14 02:42:19 +0400

    2 Inserted assets and prepared main.css with values to work with

---
+++ 2-1-skilled-elearning-page/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: ["./src/css/main.css", "./src/index.html"],
+  output: {
+    // publicPath: "/",
+    path: path.resolve(__dirname, "build"),
+    filename: "main.js",
+    assetModuleFilename: "images/[name][ext]",
+    clean: true,
+  },
+  devtool: "source-map",
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["@babel/preset-env"] },
+        },
+      },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+      { test: /\.(png|svg|jpeg|gif|jpg)$/, type: "asset/resource" },
+      { test: /\.html$/, use: ["html-loader"] },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      favicon: "./src/assets/favicon-32x32.png",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].css",
+    }),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+};
